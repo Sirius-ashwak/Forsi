05528f2d9d572ab50e41c314ed2dadf918025e4f
Sirius <mohamedashwak2022@gmail.com>
2025-06-30 01:18:02 +0530

    Fix JSX closing tag error in Settings component

--- src/components/dashboard/views/Settings.tsx
+++ src/components/dashboard/views/Settings.tsx
@@ -584,7 +584,7 @@ export const Settings: React.FC = () => {
               className="bg-gray-900 border border-gray-800 rounded-xl p-8"
             >
               {renderTabContent()}
-            </div>
+            </motion.div>
           </div>
         </div>
       </motion.div>
